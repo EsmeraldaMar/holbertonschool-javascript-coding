edccc3dee12f9dfae4aba49b229ff2eef12985a1
EsmeraldaMar <6580@holbertonstudents.com>
2024-01-22 15:20:19 +1100

    added task 1

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const fs = require('fs');
+const file = process.argv[2];
+const content = process.argv[3];
+fs.writeFile(file, content, 'utf-8', function (err, data) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(data);
+}
+);
