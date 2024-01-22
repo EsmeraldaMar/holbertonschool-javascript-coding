baa7fe2b228ef3492a38d378abb4e9d7bbbc147e
EsmeraldaMar <6580@holbertonstudents.com>
2024-01-22 14:51:11 +1100

    added readme and task 0

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const { data } = require('browserslist');
+const fs = require('fs');
+const file = process.argv[2];
+fs.readFile(file, 'utf-8', function (err, data) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(data);
+}
+);
