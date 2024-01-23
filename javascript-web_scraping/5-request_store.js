e5bcee53f4162a3731f83b81049ef11b3533202e
EsmeraldaMar <6580@holbertonstudents.com>
2024-01-23 14:09:21 +1100

    added task 5

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+const request = require('request');
+const url = 'http://loripsum.net/api';
+const fs = require('fs'); // For File IO
+const file = process.argv[3];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    fs.writeFile(file, body, 'utf-8', function (error) {
+      if (error) {
+        console.log(error);
+      }
+    }
+    );
+  }
+}
+);
